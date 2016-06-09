98425568bfcf53b6b886718943cf84edb808b696
Aaron Porter and Jordan Stringer <aaron.w.porter@gmail.com>
2016-06-08 20:42:29 -0700

    added corresponding triangle images

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,17 @@ $(document).ready(function() {
 
     if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
       result = ("Not a triangle");
+      $("#triangleImage").prop("src", "")
     } else if (side1 === side2 && side2 === side3 && side1 === side3) {
       result = ("Equilateral"); // All side equal
+      $("#triangleImage").prop("src", "https://upload.wikimedia.org/wikipedia/commons/e/e0/Equilateral-triangle.svg")
     } else if (side1 != side2 && side1 != side3) {
       result = ("Scalene"); // No side equal
+      $("#triangleImage").prop("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Triangle.Scalene.svg/640px-Triangle.Scalene.svg.png")
     }
     else {
       result = ("Isosceles"); // Two sides equal
+      $("#triangleImage").prop("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Triangle.Isosceles.svg/389px-Triangle.Isosceles.svg.png")
     }
 
     $(".panel").text(result).show();
